acfd8f7b65d3147f30db890878d7935ff74baa38
Toby Fox <tobyfox@gmail.com>
2021-05-30 21:59:49 -0700

    rename themes

--- src/themes/grunge.ts
+++ src/themes/grunge.ts
@@ -1,8 +1,8 @@
 import { ThemeOverride } from "./types";
 import { extendTheme } from "./extendTheme";
 
-export const hotWheelsTheme = extendTheme({
-  backgroundColor: "#3F3F3F",
+export const grungeTheme = extendTheme({
+  backgroundColor: "black",
   colors: {
     primary: {
       text: "rgb(215, 221, 221)",
@@ -13,84 +13,84 @@ export const hotWheelsTheme = extendTheme({
       borderRadius: "0",
       base: {
         background: "transparent",
-        border: "3px solid #009ADA",
+        border: "4px solid #eebc0f",
         text: "white",
       },
       hover: {
         background: "transparent",
-        border: "3px solid #009ADA",
+        border: "4px solid #eebc0f",
         text: "white",
       },
       active: {
         background: "transparent",
-        border: "3px solid #009ADA",
+        border: "4px solid #eebc0f",
         text: "white",
       },
       focus: {
         background: "transparent",
-        border: "3px solid #009ADA",
+        border: "4px solid #eebc0f",
         text: "white",
       },
       disabled: {
         background: "transparent",
-        border: "3px solid #eebc0f55",
-        text: "#fff55",
+        border: "4px solid #eebc0f55",
+        text: "#ffffff55",
       },
     },
     secondary: {
       borderRadius: "0",
       base: {
         background: "transparent",
-        border: "3px solid #FED103",
+        border: "4px solid rgb(24, 226, 172)",
         text: "white",
       },
       hover: {
         background: "transparent",
-        border: "3px solid #FED103",
+        border: "4px solid rgb(24, 226, 172)",
         text: "white",
       },
       active: {
         background: "transparent",
-        border: "3px solid #FED103",
+        border: "4px solid rgb(24, 226, 172)",
         text: "white",
       },
       focus: {
         background: "transparent",
-        border: "3px solid #FED103",
+        border: "4px solid rgb(24, 226, 172)",
         text: "white",
       },
       disabled: {
         background: "transparent",
-        border: "3px solid rgba(24, 226, 172, 0.33)",
-        text: "#fff55",
+        border: "4px solid rgba(24, 226, 172, 0.33)",
+        text: "#ffffff55",
       },
     },
     inverted: {
       borderRadius: "0",
       base: {
         background: "transparent",
-        border: "3px solid white",
+        border: "4px solid white",
         text: "white",
       },
       hover: {
         background: "transparent",
-        border: "3px solid white",
+        border: "4px solid white",
         text: "white",
       },
       active: {
         background: "transparent",
-        border: "3px solid white",
+        border: "4px solid white",
         text: "white",
       },
       focus: {
         background: "transparent",
-        border: "3px solid white",
+        border: "4px solid white",
         text: "white",
       },
       disabled: {
         background: "transparent",
-        border: "3px solid #fff55",
-        text: "#fff55",
+        border: "4px solid #ffffff55",
+        text: "#ffffff55",
       },
     },
   },
--- src/themes/racecar.ts
+++ src/themes/racecar.ts
@@ -1,8 +1,8 @@
 import { ThemeOverride } from "./types";
 import { extendTheme } from "./extendTheme";
 
-export const cobainTheme = extendTheme({
-  backgroundColor: "black",
+export const racecarTheme = extendTheme({
+  backgroundColor: "#3F3F3F",
   colors: {
     primary: {
       text: "rgb(215, 221, 221)",
@@ -13,84 +13,84 @@ export const cobainTheme = extendTheme({
       borderRadius: "0",
       base: {
         background: "transparent",
-        border: "4px solid #eebc0f",
+        border: "3px solid #009ADA",
         text: "white",
       },
       hover: {
         background: "transparent",
-        border: "4px solid #eebc0f",
+        border: "3px solid #009ADA",
         text: "white",
       },
       active: {
         background: "transparent",
-        border: "4px solid #eebc0f",
+        border: "3px solid #009ADA",
         text: "white",
       },
       focus: {
         background: "transparent",
-        border: "4px solid #eebc0f",
+        border: "3px solid #009ADA",
         text: "white",
       },
       disabled: {
         background: "transparent",
-        border: "4px solid #eebc0f55",
-        text: "#ffffff55",
+        border: "3px solid #eebc0f55",
+        text: "#fff55",
       },
     },
     secondary: {
       borderRadius: "0",
       base: {
         background: "transparent",
-        border: "4px solid rgb(24, 226, 172)",
+        border: "3px solid #FED103",
         text: "white",
       },
       hover: {
         background: "transparent",
-        border: "4px solid rgb(24, 226, 172)",
+        border: "3px solid #FED103",
         text: "white",
       },
       active: {
         background: "transparent",
-        border: "4px solid rgb(24, 226, 172)",
+        border: "3px solid #FED103",
         text: "white",
       },
       focus: {
         background: "transparent",
-        border: "4px solid rgb(24, 226, 172)",
+        border: "3px solid #FED103",
         text: "white",
       },
       disabled: {
         background: "transparent",
-        border: "4px solid rgba(24, 226, 172, 0.33)",
-        text: "#ffffff55",
+        border: "3px solid rgba(24, 226, 172, 0.33)",
+        text: "#fff55",
       },
     },
     inverted: {
       borderRadius: "0",
       base: {
         background: "transparent",
-        border: "4px solid white",
+        border: "3px solid white",
         text: "white",
       },
       hover: {
         background: "transparent",
-        border: "4px solid white",
+        border: "3px solid white",
         text: "white",
       },
       active: {
         background: "transparent",
-        border: "4px solid white",
+        border: "3px solid white",
         text: "white",
       },
       focus: {
         background: "transparent",
-        border: "4px solid white",
+        border: "3px solid white",
         text: "white",
       },
       disabled: {
         background: "transparent",
-        border: "4px solid #ffffff55",
-        text: "#ffffff55",
+        border: "3px solid #fff55",
+        text: "#fff55",
       },
     },
   },
